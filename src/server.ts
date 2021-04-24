035fcbf0330eb309ad2cfd0c830ab9f8b5a8e7c1
Eny Martins <enyalvesmartins@gmail.com>
2021-04-24 03:27:17 -0300

    Adicionando repositories e pasta/controllers pra organização do projeto

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,12 @@
 import express from "express";
 
 import "./database";
+import { routes } from "./routes";
 
 const app = express();
 
+app.use(express.json()); //pra aceitar requisições em json
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Olá NLW-05" });
-});
-
-app.post("/", (req, res) => {
-  return res.json({ message: "Usuário salvo com sucesso!"})
-})
+app.use(routes);
 
 app.listen(3333, () => console.log('Server is running'));
